6dbbd5e44e413ea27bad8228b470d5b763193731
Suvonov2000 <suvonov200@gmail.com>
2025-10-07 09:56:18 +0900

    sampleJava

---
+++ src/SampleScaner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SampleScaner {
+    public static void main(String[]args){
+        System.out.print("Samarkand");
+        int studentAgeID= new Scanner(System.in).nextInt();
+
+
+    }
+}
